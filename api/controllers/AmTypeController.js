b135722140c2db5f7f5092f7688ff706c7025d40
VanGoghsCoffee <mail@davidrochholz.de>
2015-02-05 16:29:31 +0100

    Merge

---
+++ api/controllers/AmTypeController.js
@@ -0,0 +1,11 @@
+/**
+ * AmTypeController
+ *
+ * @description :: Server-side logic for managing games
+ * @help :: See http://links.sailsjs.org/docs/controllers
+ */
+module.exports = {
+    test: function(req, res) {
+        res.json({msg: 'hello'});
+    },
+};
