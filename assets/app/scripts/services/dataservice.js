8e19eadce489123362683fc28986ed7975570553
DavidKoenig <david.koenig@fh-erfurt.de>
2015-02-18 10:45:54 +0100

    stuff

--- assets/app/scripts/services/dataservice.js
+++ assets/app/scripts/services/dataservice.js
@@ -13,7 +13,7 @@ angular.module('equilibrium')
     var modules = {
       apple: {
         name: 'Applemarket',
-        url:  'http://localhost:1338/#'//'http://192.168.0.198:9000/#/' //
+        url:  'http://192.168.0.198:9000/#/' //'http://localhost:1338/#'
       },
 
       exitEntry: {
