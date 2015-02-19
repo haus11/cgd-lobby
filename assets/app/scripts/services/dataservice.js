cc7781542be0a1d57a654e13e6e6e5e50a456e28
DavidKoenig <david.koenig@fh-erfurt.de>
2015-02-19 04:16:45 +0100

    merge

--- assets/app/scripts/services/dataservice.js
+++ assets/app/scripts/services/dataservice.js
@@ -13,7 +13,9 @@ angular.module('equilibrium')
     var modules = {
       apple: {
         name: 'Applemarket',
-        url:  'http://192.168.0.198:9000/#/' //'http://localhost:1338/#'
+        //url:  'http://192.168.0.198:9000/#/' //'http://localhost:1338/#'
+        url:  'http://localhost:1338/#'
+
         //url:  'http://apple.haus11.org/'//'http://192.168.0.198:9000/#/' //
 
       },
